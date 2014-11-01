08ab65c1a094aeac29a22f3f258ecb8bcb9e0e34
Alexei Pesic <aly.pesic@gmail.com>
2014-11-01 12:42:39 -0700

    Initial commit

---
+++ app/controllers/ships_controller.rb
@@ -0,0 +1,32 @@
+class ShipsController < ApplicationController
+#     Prefix Verb   URI Pattern           Controller#Action
+#       root GET    /                     ships#index
+#      ships POST   /ships(.:format)      ships#create
+#  new_ships GET    /ships/new(.:format)  ships#new
+# edit_ships GET    /ships/edit(.:format) ships#edit
+#            GET    /ships(.:format)      ships#show
+#            PATCH  /ships(.:format)      ships#update
+#            PUT    /ships(.:format)      ships#update
+#            DELETE /ships(.:format)      ships#destroy
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
---
+++ db/migrate/20141101192958_create_ships.rb
@@ -0,0 +1,21 @@
+class CreateShips < ActiveRecord::Migration
+  def change
+    create_table :ships do |t|
+      t.string :name
+      t.string :flag
+      t.integer :imonumber
+      t.integer :mmsinumber
+      t.string :publicurl
+      t.string :type
+      t.string :destination
+      t.string :etatime
+      t.string :heading
+      t.string :latitude
+      t.string :longitude
+      t.string :location
+      t.string :navigationstatus
+      t.string :photos
+      t.timestamps
+    end
+  end
+end
